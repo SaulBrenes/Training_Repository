7c5e20e324cf646142ea16bb61ef29f2111445a6
SaulBrenes <luissaulbrnschvz@gmail.com>
2023-06-03 00:32:16 -0600

    tipos de datos

---
+++ S6-JS/tipos-datos.js
@@ -0,0 +1,15 @@
+let indefinido;
+console.log(indefinido);
+
+//tipo cadena de caracteres
+let texto = 'Hello, world!';
+console.log(typeof(texto));
+
+//tipo números
+let entero = 45;
+console.log(typeof(entero));
+
+let decimal = 4.5;
+console.log(typeof(decimal));
+
+console.log(typeof(true));
