5dda0e1fb37194d96f9879be75ea9f109c06e91b
SaulBrenes <luissaulbrnschvz@gmail.com>
2023-06-04 22:10:07 -0600

    casteo de números

---
+++ S6-JS/casting-dato.js
@@ -0,0 +1,23 @@
+let dato = 15;
+console.log(typeof(dato));
+
+//dato = String(dato);
+dato = dato.toString()
+console.log(typeof(dato));
+
+dato = '21'
+console.log(typeof(dato));
+
+dato = Number(dato);
+dato = parseInt(dato);
+console.log(typeof(dato));
+
+dato = '4.5';
+dato = parseFloat(dato);
+console.log(typeof(dato));
+console.log(dato)
+
+
+
+
+
