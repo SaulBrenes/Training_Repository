56d905ec66d2d267c7c7e59ca34f12faf91f5264
SaulBrenes <luissaulbrnschvz@gmail.com>
2023-06-05 22:09:17 -0600

    Ejercicio de suma

---
+++ S6-JS/suma.js
@@ -0,0 +1,11 @@
+//Entrada de los datos
+let a = prompt('Ingrese un número:');
+let b = prompt('Ingrese un número:');
+
+a = Number(a);
+b = Number(b);
+//Operación
+let suma = a+b;
+
+//Mostrar resultado
+document.write('La suma es:', suma);
