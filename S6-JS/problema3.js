c6430c5fe0b1ea2ba2061367c69c3bf8103b7483
SaulBrenes <luissaulbrnschvz@gmail.com>
2023-06-10 07:10:22 -0600

    Se termina sección 6

--- S6-JS/problema3.js
+++ S6-JS/problema3.js
@@ -2,11 +2,21 @@
 let vv, igv, pv;
 
 //Entradas
-vv = Number(prompt('Ingrese el valor de la venta del producto: '));
+vv = Number(prompt("Ingrese el valor de la venta del producto: "));
 
 //Proceso
 igv = vv * 0.19;
 pv = vv + igv;
 
+/*Salida de datos
+document.write('El valor de venta es: ', vv);
 document.write('El IGV(19%) es igual a: ', igv);
 document.write('<br>El precio de venta es de: ', pv)
+*/
+document.write('<h1>Salida de datos</h1>')
+document.write(`<pre>
+                Valor de venta  : ${vv} <br>
+                IGV             : ${igv} <br>
+                Precio de venta : ${pv}
+                </pre>
+`);
