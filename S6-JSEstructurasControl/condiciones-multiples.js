e474f78aa51f67058cbffb12f8b26a6f5b70ea94
SaulBrenes <luissaulbrnschvz@gmail.com>
2023-06-12 01:23:05 -0600

    condiciones multiples

---
+++ S6-JSEstructurasControl/condiciones-multiples.js
@@ -0,0 +1,17 @@
+let v = 'A', print = '';
+
+if(v=== 'a' || v === 'A'){
+    print += `${v} es VOCAL`
+}else if(v=== 'e' || v === 'E'){
+    print += `${v} es VOCAL`
+}else if(v=== 'i' || v === 'I'){
+    print += `${v} es VOCAL`
+}else if(v=== 'o' || v === 'O'){
+    print += `${v} es VOCAL`
+}else if(v=== 'u' || v === 'U'){
+    print += `${v} es VOCAL`
+}else{
+    print += `${v} no es VOCAL`
+}
+
+console.log(print);
